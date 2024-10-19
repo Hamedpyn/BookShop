062a4aa5538a5ae65fdde0a072c152880b474b09
Hamed <hamedpyn@gmail.com>
2024-10-19 22:26:41 +0330

    Add menu items with hover effect

--- book-shop/src/Components/NavBar/NavBar.jsx
+++ book-shop/src/Components/NavBar/NavBar.jsx
@@ -12,6 +12,33 @@ export default function NavBar() {
           کتاب ها
         </NavLink>
       </li>
+      <li className="py-[62px] group relative  group">
+        <a className="underEffect border-l-2 h-4 flex items-center cursor-pointer hover:text-current transition-all border-[#f6f7f9] px-2 gap-1">
+          <span>مجموعه</span>
+          <span>
+            <FaChevronDown className="group-hover:rotate-180 transition-all" />
+          </span>
+        </a>
+
+      </li>
+      <li className="py-[62px] relative group">
+        <a className="cursor-pointer hover:text-current transition-all underEffect border-l-2 h-4 flex items-center border-[#f6f7f9] px-2 gap-1">
+          <span>دسته بندی</span>
+          <span>
+            <FaChevronDown className="group-hover:rotate-180 transition-all" />
+          </span>
+        </a>
+
+      </li>
+      <li className="py-[62px] relative group">
+        <a className="cursor-pointer hover:text-current transition-all underEffect border-l-2 h-4 flex items-center border-[#f6f7f9] px-2 gap-1">
+          <span>صفحات</span>
+          <span>
+            <FaChevronDown className="group-hover:rotate-180 transition-all" />
+          </span>
+        </a>
+        </ul>
+      </li>
       <li className="relative py-[58px] ">
         <NavLink to={"/contact-us"} className={({ isActive }) => (isActive ? "isActive px-2" : "cursor-pointer hover:text-current transition-all underEffect px-2")}>
           تماس با ما
