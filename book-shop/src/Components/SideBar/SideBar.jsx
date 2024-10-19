a2c8928cf0395f2bb31813b8134e6af089b64d82
Hamed <hamedpyn@gmail.com>
2024-10-19 21:14:39 +0330

    manage sidebar toggle and body overflow using useEffect
    - Added logic to toggle sidebar visibility by adding/removing the "wrapper" class.
    - Disabled body scrolling by applying "overflow-hidden" when the sidebar is open.
    - Added event listener to close the sidebar on body click.
    - Cleaned up event listener on component unmount or sidebar close.

--- book-shop/src/Components/SideBar/SideBar.jsx
+++ book-shop/src/Components/SideBar/SideBar.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useRef, useState } from "react";
 export default function SideBar(sideBarObject) {
     let wrapperRef = useRef();
+    let bodyRef = useRef(null);
+
+    useEffect(() => {
+        bodyRef.current = document.body;
+        if (sideBarObject.isTrue) {
+            wrapperRef.current.classList.add("wrapper");
+            bodyRef.current.classList.add("overflow-hidden");
+            bodyRef.current.addEventListener("click", sideBarObject.toggleMenu);
+        } else {
+            wrapperRef.current.classList.remove("wrapper");
+            bodyRef.current.classList.remove("overflow-hidden");
+        }
+
+        return () => {
+            bodyRef.current.removeEventListener("click", sideBarObject.toggleMenu);
+        };
+    }, [sideBarObject.isTrue]);
+
     return (
         <>
             <div className={`z-50 fixed top-0 right-0 transition-all ease-out duration-300 h-full w-[270px] sm:w-[350px] bg-current overflow-auto overflow-x-hidden lg:invisible lg:opacity-0 ${sideBarObject.isTrue ? "translate-x-0 visible opacity-100" : "translate-x-full invisible opacity-0"}`}
