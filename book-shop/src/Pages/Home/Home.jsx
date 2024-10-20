4fa6cea3c5d4136e24bdbbf6bb7f520e3a0d5642
Hamed <hamedpyn@gmail.com>
2024-10-21 02:50:19 +0330

    Import Collection component in Home Page

--- book-shop/src/Pages/Home/Home.jsx
+++ book-shop/src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Collection from "../../Components/Collection/Collection";
 import MainHome from "../../Components/MainHome/MainHome";
 
 export default function Home() {
   return (
     <>
       <MainHome />
+      <Collection />
     </>
   )
 }
