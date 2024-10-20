b9bfa4bd5b65e7b7753acef41bffdd40f07df5ae
Hamed <hamedpyn@gmail.com>
2024-10-21 03:01:23 +0330

    Set the pages title by using ComponentTitle,jsx

--- book-shop/src/Components/Collection/Collection.jsx
+++ book-shop/src/Components/Collection/Collection.jsx
@@ -1,5 +1,11 @@
+import ComponentsTitle from "../ComponentsTitle/ComponentsTitle";
 
 export default function Collection() {
     return (
+        <div className="flex items-center flex-col w-full justify-center">
+            <div>
+                <ComponentsTitle tag={"دسته بندی"} title={"مجموعه کتاب"} extraText={"اکسترا"} />
+            </div>
+        </div>
     )
 }
