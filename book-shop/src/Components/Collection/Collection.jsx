34c7db3d8ddeee637824598d0e597a8485f05ae8
Hamed <hamedpyn@gmail.com>
2024-10-21 03:06:33 +0330

    Top-left side of Collection component
    -Because of the unique wrap, I have to put each two of the left side's cards in a div

--- book-shop/src/Components/Collection/Collection.jsx
+++ book-shop/src/Components/Collection/Collection.jsx
@@ -15,6 +15,22 @@ export default function Collection() {
                         <img src="/Images/book2.png" className="absolute bottom-0 z-0 w-full h-full object-cover" alt="action" />
                     </div>
                 </div>
+                <div className="md:h-full flex-wrap flex flex-col md:justify-between items-center">
+
+                    <div className="flex items-center flex-col md:w-full md:items-start md:flex-row gap-7">
+                        <div className="relative bg-[#26292A] flex items-center md:items-start flex-col w-11/12 classic md:w-1/2 md:h-[260px] rounded-[40px] overflow-hidden">
+                            <div className="absolute md:right-[30px] top-[30px]">
+                                <CollectionsButton title={"کلاسیک"} />
+                            </div>
+                            <img className="w-full h-full object-cover" src="/Images/book3.png" alt="classic" />
+                        </div>
+                        <div className="relative bg-[#FF6600] flex items-center flex-col w-11/12 md:w-1/2 md:h-[230px] overflow-hidden rounded-[40px]">
+                            <div className="absolute md:right-[30px] top-[30px]">
+                                <CollectionsButton title={"کارگاه رمز و راز"} />
+                            </div>
+                            <img className="w-full h-full object-cover md:pr-10" src="/Images/book1.png" alt="secret" />
+                        </div>
+                    </div>
             </div>
         </div>
     )
