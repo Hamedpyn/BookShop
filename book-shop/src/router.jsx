f7deddb9917bf6ae8357a8f07737eaf4d85957e2
Hamed <hamedpyn@gmail.com>
2024-10-21 02:46:52 +0330

    Changed component route & delete useless routes

--- book-shop/src/router.jsx
+++ book-shop/src/router.jsx
@@ -1,9 +1,7 @@
-import Books from "./Pages/Books/Books";
 import Home from "./Pages/Home/Home";
 
 const router = [
     { path: "/", element: <Home /> },
-    { path: "/books", element: <Books /> },
 ]
 
 export default router
