8d52ea03e0e5b005f929f89c8d8798f9eb33bb72
Hamed <hamedpyn@gmail.com>
2024-10-21 22:56:24 +0330

    Books Details for booksPreView.jsx
    The Components name has changed because it was going to be used for more than one specific detail

---
+++ book-shop/src/Components/datas/Datas.jsx
@@ -0,0 +1,19 @@
+const subMenuDetail =[
+    { id: 1, img: "/Images/package2-1.jpg", title: "کتاب های بیوگرافی", btnColor: "#b2824c" },
+    { id: 2, img: "/Images/package3-1.jpg", title: "کتاب های هنر و گرافیک", btnType: "success" },
+    { id: 3, img: "/Images/book-9.jpg", title: "کتاب های ارز مجازی", btnType: "warning" },
+    { id: 4, img: "/Images/book-10.jpg", title: "کتاب های تاریخی", btnType: "dark" },
+];
+
+const bookPreView = [
+    {id:1,title:"صلیب بانو",price:99,img:"/Images/book-10-600x600.jpg"},
+    {id:2,title:"افعی در چنجره",price:35,img:"/Images/package4-1-600x600.jpg"},
+    {id:3,title:"هرگز رویا را متوفق نکنید",price:44,img:"/Images/package8-1-600x600.jpg"},
+    {id:4,title:"سیارات",price:87,img:"/Images/package-11-600x600.jpg"},
+    {id:5,title:"زندگی نامه",price:20,img:"/Images/package2-1-600x600.jpg"},
+    {id:6,title:"دنباله کتابها",price:79,img:"/Images/package-12-600x600.jpg"},
+    {id:7,title:"شبکه R",price:22,img:"/Images/package5-1-600x600.jpg"},
+    {id:8,title:"غار",price:18,img:"/Images/package6-1-600x600.jpg"},
+]
+
+export {subMenuDetail,bookPreView}
--- book-shop/src/Components/datas/subMenuDetails.jsx
+++
@@ -1,8 +0,0 @@
-const subMenuDetail =[
-    { id: 1, img: "/Images/package2-1.jpg", title: "کتاب های بیوگرافی", btnColor: "#b2824c" },
-    { id: 2, img: "/Images/package3-1.jpg", title: "کتاب های هنر و گرافیک", btnType: "success" },
-    { id: 3, img: "/Images/book-9.jpg", title: "کتاب های ارز مجازی", btnType: "warning" },
-    { id: 4, img: "/Images/book-10.jpg", title: "کتاب های تاریخی", btnType: "dark" },
-];
-
-export {subMenuDetail}
