a014aa795c834d57de619a2c981008fe3904b0ba
Hamed <hamedpyn@gmail.com>
2024-10-21 22:53:51 +0330

    Added a New font-family

--- book-shop/src/Components/Header/Header.jsx
+++ book-shop/src/Components/Header/Header.jsx
@@ -61,7 +61,7 @@ function Header() {
                     </div>
                     <div className="menuBasis md:relative justify-end items-center gap-4 relative lg:basis-auto lg:h-[140px] lg:mr-3 lg:justify-center lg:w-[200px] basket xl:mr-10">
                         <FaBasketShopping className="menuButton bg-[#f8f8fa] text-gray-700 border border-gray-300" />
-                        <span id="fontF" className="absolute bottom-0 md:left-[185px] lg:bottom-[48px] 
+                        <span className="fontF absolute bottom-0 md:left-[185px] lg:bottom-[48px] 
                          md:bottom-[5px] lg:left-[155px] bg-red-600 rounded-full p-1 h-4 flex justify-center items-center text-xs">0</span>
                         <button className="hidden md:flex rounded-full bg-current items-center hover:bg-black transition-all duration-200 px-6 py-4 lg:py-0 lg:text-sm lg:h-[60px] gap-2">
                             <FaUserAlt />
