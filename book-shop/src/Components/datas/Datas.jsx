c145461e9d28c1974625b4463b0a17428aac55c8
Hamed <hamedpyn@gmail.com>
2024-10-23 01:07:02 +0330

    Added an array of user comments with their details

--- book-shop/src/Components/datas/Datas.jsx
+++ book-shop/src/Components/datas/Datas.jsx
@@ -1,4 +1,4 @@
-const subMenuDetail =[
+const subMenuDetail = [
     { id: 1, img: "/Images/package2-1.jpg", title: "کتاب های بیوگرافی", btnColor: "#b2824c" },
     { id: 2, img: "/Images/package3-1.jpg", title: "کتاب های هنر و گرافیک", btnType: "success" },
     { id: 3, img: "/Images/book-9.jpg", title: "کتاب های ارز مجازی", btnType: "warning" },
@@ -6,14 +6,25 @@ const subMenuDetail =[
 ];
 
 const bookPreView = [
-    {id:1,title:"صلیب بانو",price:99,img:"/Images/book-10-600x600.jpg"},
-    {id:2,title:"افعی در پنجره",price:35,img:"/Images/package4-1-600x600.jpg"},
-    {id:3,title:"هرگز رویا را متوقف نکنید",price:44,img:"/Images/package8-1-600x600.jpg"},
-    {id:4,title:"سیارات",price:87,img:"/Images/book-11-600x600.jpg"},
-    {id:5,title:"زندگی نامه",price:20,img:"/Images/package2-1-600x600.jpg"},
-    {id:6,title:"دنباله کتابها",price:79,img:"/Images/book-12-600x600.jpg"},
-    {id:7,title:"شبکه R",price:22,img:"Images/package5-1-600x600-1.jpg"},
-    {id:8,title:"غار",price:18,img:"/Images/package6-1-600x600.jpg"},
+    { id: 1, title: "صلیب بانو", price: 99, img: "/Images/book-10-600x600.jpg" },
+    { id: 2, title: "افعی در پنجره", price: 35, img: "/Images/package4-1-600x600.jpg" },
+    { id: 3, title: "هرگز رویا را متوقف نکنید", price: 44, img: "/Images/package8-1-600x600.jpg" },
+    { id: 4, title: "سیارات", price: 87, img: "/Images/book-11-600x600.jpg" },
+    { id: 5, title: "زندگی نامه", price: 20, img: "/Images/package2-1-600x600.jpg" },
+    { id: 6, title: "دنباله کتابها", price: 79, img: "/Images/book-12-600x600.jpg" },
+    { id: 7, title: "شبکه R", price: 22, img: "Images/package5-1-600x600-1.jpg" },
+    { id: 8, title: "غار", price: 18, img: "/Images/package6-1-600x600.jpg" },
 ]
 
-export {subMenuDetail,bookPreView}
+const usersComments = [
+    { id: 1, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-1.png", userName: "علی حسینی", userJob: "کارآفرین" },
+    { id: 2, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-2.jpg", userName: "سارا بهرامی", userJob: "معلم" },
+    { id: 3, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-2.jpg", userName: "مریم هاشمی", userJob: "مجری" },
+    { id: 4, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-3.jpg", userName: "سپهر آدرماهی", userJob: "برنامه نویس" },
+    { id: 5, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-4.jpg", userName: "زهرا جعفری", userJob: "تکنسین" },
+    { id: 6, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/photo_2024-10-22_19-54-43.jpg", userName: "حسین غلامی", userJob: "پرستار" },
+    { id: 7, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/photo_2024-10-22_19-55-44.jpg", userName: "امیرحسین خرمیری", userJob: "راننده" },
+    { id: 8, title: "“بهترین. واقعاً خوب. ای کاش اول به آن فکر می کردم. فروشگاه اکسترا را به شما هم پیشنهاد می کنم.”", img: "/Images/testimonial-1.png", userName: "دانیال رستمی", userJob: "مدیر کسب و کار" },
+]
+
+export { subMenuDetail, bookPreView, usersComments }
