5f1654412e269517b422971ea19a773f948504b5
Hamed <hamedpyn@gmail.com>
2024-10-22 14:41:14 +0330

    Import Customer Slider component

---
+++ book-shop/src/Components/CustomersSlider/CustomersSlider.jsx
@@ -0,0 +1,14 @@
+import ComponentsTitle from '../ComponentsTitle/ComponentsTitle'
+
+export default function CustomersSlider() {
+  return (
+    <div>
+      <div className="">
+        <ComponentsTitle tag={"نظرات و پیشنهادات"} title={"مشتریان ما چه می گویند؟"} />
+      </div>
+      <div className="">
+        {/* Slider */}
+      </div>
+    </div>
+  )
+}
