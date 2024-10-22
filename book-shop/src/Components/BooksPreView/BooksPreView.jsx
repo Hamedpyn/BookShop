b2e5b59093d1e5bc8de1b1c17d80b32985368159
Hamed <hamedpyn@gmail.com>
2024-10-22 14:39:14 +0330

    Made a map on books details and Added a button

--- book-shop/src/Components/BooksPreView/BooksPreView.jsx
+++ book-shop/src/Components/BooksPreView/BooksPreView.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ComponentsTitle from "../ComponentsTitle/ComponentsTitle";
 import { bookPreView } from "../datas/Datas";
+import BookCard from "../BookCard/BookCard";
+import { NavLink } from "react-router-dom";
 
 export default function BooksPreView() {
     const [books,] = useState(bookPreView)
@@ -17,6 +19,15 @@ export default function BooksPreView() {
                     <ComponentsTitle tag={"پر فروش ترین ها"} title={"پر فروش ترین های"} extraText={"امسال"} />
                 </div>
                 <div className="flex flex-col items-center mb-40 gap-16 flex-wrap md:flex-row justify-center">
+                    {books.map(item => (
+                        <BookCard key={item.id} {...item} />
+                    ))}
+                </div>
+                <NavLink to={"books"} className={({ isActive }) => (isActive ? "" : "")}>
+                    <button className="bg-current flex py-4 px-6 rounded-full text-white cursor-pointer hover:bg-black transition-all">
+                    مشاهده کل مجموعه
+                    </button>
+                </NavLink>
             </div>
             <svg className="rotate-180" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1600 172">
                 <path style={{ fill: "transparent" }} d="M-3,108.5S398.468,0.5,799.951.5C1201.47,0.5,1603,108.5,1603,108.5v203H-3v-203Z"></path>
