e8613b789e726976de6f7b9a4b476f18c5ed389d
Hamed <hamedpyn@gmail.com>
2024-10-24 20:41:22 +0330

    Import Contact.jsx in router

--- book-shop/src/router.jsx
+++ book-shop/src/router.jsx
@@ -1,7 +1,9 @@
 import Home from "./Pages/Home/Home";
+import ContactUs from "./Pages/ContactUs/ContactUs";
 
 const router = [
     { path: "/", element: <Home /> },
+    { path: "/ContactUs", element: <ContactUs /> },
 ]
 
 export default router
