5c6a18e115a2e7290f8eed742f5b9b8fdcc450bc
Hamed <hamedpyn@gmail.com>
2024-10-25 16:23:10 +0330

    Import Books Page

---
+++ book-shop/src/Pages/Books/Books.jsx
@@ -0,0 +1,11 @@
+import BreadCrumb from "../../Components/BreadCrumb/BreadCrumb";
+
+export default function Books() {
+  return (
+    <div className="">
+        <div className="">
+            <BreadCrumb title={"فروشگاه"} tag={"محصولات"} />
+        </div>
+    </div>
+  )
+}
--- book-shop/src/router.jsx
+++ book-shop/src/router.jsx
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home/Home";
+import Books from "./Pages/Books/Books";
 import ContactUs from "./Pages/ContactUs/ContactUs";
 
 const router = [
     { path: "/", element: <Home /> },
     { path: "/ContactUs", element: <ContactUs /> },
+    { path: "/Books", element: <Books /> },
 ]
 
 export default router
