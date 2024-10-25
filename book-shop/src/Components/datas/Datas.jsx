645b50356cbf571f164cc5794ae97bce958e44a7
Hamed <hamedpyn@gmail.com>
2024-10-25 19:10:28 +0330

    Set an array for the whole items since we dont have API

--- book-shop/src/Components/datas/Datas.jsx
+++ book-shop/src/Components/datas/Datas.jsx
@@ -28,16 +28,31 @@ const usersComments = [
 ]
 
 const authorsDetails = [
-    { id: 1, title: "ساناز بهرامی",job:"نویسنده ایرانی", img: "/Images/team-4.jpg" },
-    { id: 2, title: "جیمز جان",job:"روانشناس", img: "/Images/team-3.jpg" },
-    { id: 3, title: "مایک براون",job:"نویسنده برزیلی", img: "/Images/team-2.jpg" },
-    { id: 4, title: "آدام گرین",job:"روانشناس", img: "/Images/team-member-1.jpg"},
+    { id: 1, title: "ساناز بهرامی", job: "نویسنده ایرانی", img: "/Images/team-4.jpg" },
+    { id: 2, title: "جیمز جان", job: "روانشناس", img: "/Images/team-3.jpg" },
+    { id: 3, title: "مایک براون", job: "نویسنده برزیلی", img: "/Images/team-2.jpg" },
+    { id: 4, title: "آدام گرین", job: "روانشناس", img: "/Images/team-member-1.jpg" },
 ]
 
 const footerSubMenu = [
-    {id:1,title:"منو سفارشی",firstSubMenu:"صفحه اصلی",secSubMenu:'کتاب ها',thirdSubMenu:"محموعه",forthSubMenu:"دسته بندی"},
-    {id:2,title:"پشتیبانی",firstSubMenu:"سوالات متداول",secSubMenu:'قوانین',thirdSubMenu:"سیاست ها و قوانین",forthSubMenu:"گزارش"},
-    {id:3,title:"درباره ما",firstSubMenu:"درباره ما",secSubMenu:'شرکا',thirdSubMenu:"چشم انداز",forthSubMenu:"تماس با ما"},
+    { id: 1, title: "منو سفارشی", firstSubMenu: "صفحه اصلی", secSubMenu: 'کتاب ها', thirdSubMenu: "محموعه", forthSubMenu: "دسته بندی" },
+    { id: 2, title: "پشتیبانی", firstSubMenu: "سوالات متداول", secSubMenu: 'قوانین', thirdSubMenu: "سیاست ها و قوانین", forthSubMenu: "گزارش" },
+    { id: 3, title: "درباره ما", firstSubMenu: "درباره ما", secSubMenu: 'شرکا', thirdSubMenu: "چشم انداز", forthSubMenu: "تماس با ما" },
 ]
 
-export { subMenuDetail, bookPreView, usersComments, authorsDetails,footerSubMenu }
+const allBooksDetails = [
+    { id: 1, title: "5 سال", img: "/Images/book-9.jpg", price: 69 },
+    { id: 2, title: "افعی در پنجره", price: 35, img: "/Images/package4-1-600x600.jpg" },
+    { id: 3, title: "تولد", img: "/Images/package2-1.jpg", price: 35 },
+    { id: 4, title: "دنباله کتابها", price: 79, img: "/Images/book-12-600x600.jpg" },
+    { id: 5, title: "رودخانه اکسترا", price: 15, img: "/Images/package1-1-600x600.jpg" },
+    { id: 6, title: "زندگی نامه", price: 20, img: "/Images/package2-1.jpg" },
+    { id: 7, title: "سیارات", price: 87, img: "/Images/book-11-600x600.jpg" },
+    { id: 8, title: "شبکه R", price: 22, img: "Images/package5-1-600x600-1.jpg" },
+    { id: 9, title: "صلیب بانو", price: 99, img: "/Images/book-10-600x600.jpg" },
+    { id: 10, title: "غار", price: 18, img: "/Images/package6-1-600x600.jpg" },
+    { id: 11, title: "لوازم آرایشی طبیعی", price: 18, img: "/Images/package3-1.jpg" },
+    { id: 12, title: "هرگز رویا را متوقف نکنید", price: 44, img: "/Images/package8-1-600x600.jpg" },
+]
+
+export { subMenuDetail, bookPreView, usersComments, authorsDetails, footerSubMenu, allBooksDetails }
