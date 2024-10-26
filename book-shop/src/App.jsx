36e3d13a3e719f0462709d22c61065a5a31b6f0a
Hamed <hamedpyn@gmail.com>
2024-10-26 03:38:55 +0330

    create UserBasketContext for managing user basket state across components

--- book-shop/src/App.jsx
+++ book-shop/src/App.jsx
@@ -4,15 +4,18 @@ import { useRoutes } from 'react-router-dom'
 import routes from './router'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
-
+import UserBasketContext from "./Contexts/UserBasketContext"
+import { useState } from 'react';
 function App() {
+  const [bookBasket,setBookBasket]=useState([])
+
   let router = useRoutes(routes)
   return (
-    <>
+    <UserBasketContext.Provider value={{bookBasket,setBookBasket}}>
       <Header />
       {router}
       <Footer />
-    </>
+    </UserBasketContext.Provider>
   )
 }
 
---
+++ book-shop/src/Contexts/UserBasketContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react"
+
+let UserBasketContext = createContext()
+export default UserBasketContext
