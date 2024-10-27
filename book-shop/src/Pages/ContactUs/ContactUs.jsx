c197041c5a64f7953289969ef8729019a010fc45
Hamed <hamedpyn@gmail.com>
2024-10-28 03:17:01 +0330

    Add Framer Motion animations to ContactUs& Home Page

--- book-shop/src/Pages/ContactUs/ContactUs.jsx
+++ book-shop/src/Pages/ContactUs/ContactUs.jsx
@@ -1,15 +1,33 @@
 import BreadCrumb from "../../Components/BreadCrumb/BreadCrumb";
 import ContactsDetails from "../../Components/ContactsDetails/ContactsDetails";
 import UserTicket from "../../Components/UserTicket/UserTicket";
-
+import { motion } from "framer-motion";
 
 export default function ContactUs() {
   window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   return (
     <>
-      <BreadCrumb title={"تماس با ما"} tag={"تماس با ما"} />
-      <ContactsDetails />
-      <UserTicket />
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }}>
+        <BreadCrumb title={"تماس با ما"} tag={"تماس با ما"} />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }}>
+        <ContactsDetails />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }}>
+        <UserTicket />
+      </motion.div>
     </>
   )
 }
--- book-shop/src/Pages/Home/Home.jsx
+++ book-shop/src/Pages/Home/Home.jsx
@@ -7,23 +7,61 @@ import MainHome from "../../Components/MainHome/MainHome";
 import SpecialDiscount from "../../Components/SpecialDiscount/SpecialDiscount";
 import { useContext } from "react";
 import UserBasketContext from "../../Contexts/UserBasketContext";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const { isModal } = useContext(UserBasketContext);
 
   return (
     <>
-      <MainHome />
-      <Collection />
-      <BooksPreView />
-      <CustomersSlider />
-      <SpecialDiscount />
-      <AuthorsInfos />
+      <motion.div
+        initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: false }}>
+        <MainHome />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: false }}>
+        <Collection />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y:50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: false }}>
+        <BooksPreView />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: false }}>
+        <CustomersSlider />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: false }}>
+        <SpecialDiscount />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }} >
+        <AuthorsInfos />
+    </motion.div>
       <div className={`z-50 bold fixed top-[10px] right-[10px] transition-all ease-in-out duration-300 ${isModal ? "translate-x-0 visible opacity-100" : "translate-x-full invisible opacity-0"}`}>
         <Alert color="success" withBorderAccent>
           <span className="font-medium">محصول مورد نظر شما با موفقیت به سبد خرید اضافه شد.</span>
         </Alert>
       </div>
     </>
+
   )
 }
