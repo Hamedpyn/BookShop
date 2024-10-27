522b339fbbd2e980daf43e6abb61c5111eca3f63
Hamed <hamedpyn@gmail.com>
2024-10-27 04:18:53 +0330

    add conditionalTag to BreadCrumb component for enhanced navigation
    - Added `conditionalTag` prop to display an additional breadcrumb link when provided.
    - Updated breadcrumb structure to conditionally show `conditionalTag` with separator icon, improving navigation flexibility.
    - Refactored component layout to maintain consistent appearance whether `conditionalTag` is present or not.

--- book-shop/src/Components/BreadCrumb/BreadCrumb.jsx
+++ book-shop/src/Components/BreadCrumb/BreadCrumb.jsx
@@ -1,21 +1,47 @@
+/* eslint-disable react/prop-types */
 import { AiFillHome } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 
-export default function BreadCrumb({ title, tag }) {
+export default function BreadCrumb({ title, tag, conditionalTag }) {
     return (
         <div className="w-full breadCrumb bg-[#F8F8FA] h-[100px] flex items-end justify-start md:justify-between">
             <div className="md:w-11/12 justify-between pb-5 flex items-center">
                 <h2 className="kalameh text-2xl pr-5 xl:pr-12 navbar-logo">{title}</h2>
                 <div className="hidden md:flex items-center flex-row-reverse gap-2">
-                    <div>
-                        <span
-                            className="text-sm text-[#333]"
-                        >{tag}</span>
-                    </div>
-                    <div>
-                        <FaChevronLeft className="text-[12px] text-[#333]" />
-                    </div>
+                    {conditionalTag ? (
+                        <div className="hidden md:flex items-center flex-row-reverse gap-2">
+                            <div>
+                                <span
+                                    className="text-sm text-[#333]"
+                                >{conditionalTag}</span>
+                            </div>
+                            <div>
+                                <FaChevronLeft className="text-[12px] text-[#333]" />
+                            </div>
+                            <Link to={"/Books"}>
+                                <span
+                                    className="text-sm text-[#333]"
+                                >{tag}</span>
+                            </Link>
+                            <div>
+                                <FaChevronLeft className="text-[12px] text-[#333]" />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="hidden md:flex items-center flex-row-reverse gap-2">
+                            <div>
+                                <span
+                                    className="text-sm text-[#333]"
+                                >{tag}</span>
+                            </div>
+                            <div>
+                                <FaChevronLeft className="text-[12px] text-[#333]" />
+                            </div>
+                        </div>
+                    )}
+
+
                     <div>
                         <Link to={"/"}>
                             <AiFillHome className="text-lg" />
