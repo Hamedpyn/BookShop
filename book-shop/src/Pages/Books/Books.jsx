1026bec5f8a6f2cc37cca817de1126330a04897b
Hamed <hamedpyn@gmail.com>
2024-10-28 03:14:36 +0330

    Add Framer Motion animations to Books.jsx

--- book-shop/src/Pages/Books/Books.jsx
+++ book-shop/src/Pages/Books/Books.jsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect, useState, useMemo, useCallback,useId } from "react";
+import { useContext, useEffect, useState, useMemo, useCallback, useId } from "react";
 import BookCard from "../../Components/BookCard/BookCard";
 import BreadCrumb from "../../Components/BreadCrumb/BreadCrumb";
 import { allBooksDetails } from "../../datas/Datas";
 import { HiArrowLongLeft, HiArrowLongRight } from "react-icons/hi2";
 import UserBasketContext from "../../Contexts/UserBasketContext";
 import { Alert } from "flowbite-react";
+import { motion } from "framer-motion";
 
 export default function Books() {
   const [books] = useState(allBooksDetails);
@@ -18,7 +19,7 @@ export default function Books() {
 
   // Scroll to top on page change
   useEffect(() => {
-    window.scrollTo({ top: 200, left: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, [currentPage]);
 
   // Filter books based on current page
@@ -47,47 +48,59 @@ export default function Books() {
         setBookBasket(prev => [newItemToBasket, ...prev]);
       }
     }
-  }, [bookBasket, setBookBasket,uniqueId]);
+  }, [bookBasket, setBookBasket, uniqueId]);
 
   return (
     <div>
       <BreadCrumb title={"فروشگاه"} tag={"محصولات"} />
 
-      <div className="flex flex-col mt-20 items-center gap-16 flex-wrap md:flex-row justify-center">
-        {filteredBooks.map(item => (
-          <BookCard onItem={() => preViewToBasket(item.id, item.title)} isTrue={true} key={item.id} {...item} />
-        ))}
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }}>
+        <div className="flex flex-col mt-20 items-center gap-16 flex-wrap md:flex-row justify-center">
+          {filteredBooks.map(item => (
+            <BookCard onItem={() => preViewToBasket(item.id, item.title)} isTrue={true} key={item.id} {...item} />
+          ))}
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false }}>
 
-      <div className="flex items-center justify-center w-full mt-24 gap-2 px-2 flex-wrap">
-        {currentPage > 1 && (
-          <button
-            onClick={() => setCurrentPage(prev => prev - 1)}
-            className="border w-[65px] h-[65px] transition-all hover:bg-current rounded-full flex items-center justify-center group"
-          >
-            <HiArrowLongRight className="text-xl text-current group-hover:text-white" />
-          </button>
-        )}
+        <div className="flex items-center justify-center w-full mt-24 gap-2 px-2 flex-wrap">
+          {currentPage > 1 && (
+            <button
+              onClick={() => setCurrentPage(prev => prev - 1)}
+              className="border w-[65px] h-[65px] transition-all hover:bg-current rounded-full flex items-center justify-center group"
+            >
+              <HiArrowLongRight className="text-xl text-current group-hover:text-white" />
+            </button>
+          )}
 
-        {pagesNumber.map(number => (
-          <button
-            onClick={() => PaginateBooks(number + 1)}
-            key={number + 1}
-            className={`border w-[65px] h-[65px] transition-all rounded-full text-sm ${currentPage === number + 1 ? "bg-current text-white" : "bg-white text-current"}`}
-          >
-            {number + 1}
-          </button>
-        ))}
+          {pagesNumber.map(number => (
+            <button
+              onClick={() => PaginateBooks(number + 1)}
+              key={number + 1}
+              className={`border w-[65px] h-[65px] transition-all rounded-full text-sm ${currentPage === number + 1 ? "bg-current text-white" : "bg-white text-current"}`}
+            >
+              {number + 1}
+            </button>
+          ))}
 
-        {currentPage < pageCount && (
-          <button
-            onClick={() => setCurrentPage(prev => prev + 1)}
-            className="border w-[65px] h-[65px] transition-all hover:bg-current rounded-full flex items-center justify-center group"
-          >
-            <HiArrowLongLeft className="text-xl text-current group-hover:text-white" />
-          </button>
-        )}
-      </div>
+          {currentPage < pageCount && (
+            <button
+              onClick={() => setCurrentPage(prev => prev + 1)}
+              className="border w-[65px] h-[65px] transition-all hover:bg-current rounded-full flex items-center justify-center group"
+            >
+              <HiArrowLongLeft className="text-xl text-current group-hover:text-white" />
+            </button>
+          )}
+        </div>
+      </motion.div>
       <div className={`z-50 bold fixed top-[10px] right-[10px] transition-all ease-out duration-300 ${isModal ? "translate-x-0 visible opacity-100" : "translate-x-full invisible opacity-0"}`}>
         <Alert color="success" withBorderAccent>
           <span className="font-medium">محصول مورد نظر شما با موفقیت به سبد خرید اضافه شد.</span>
