283a26a84094a7d7bc0bbc2d58e38de94a11b99e
Hamed <hamedpyn@gmail.com>
2024-10-30 21:52:42 +0330

    Give all buttons a path to navigate to their own page

--- src/Components/Collection/Collection.jsx
+++ src/Components/Collection/Collection.jsx
@@ -11,7 +11,7 @@ export default function Collection() {
                 <div className="flex flex-col w-full md:w-[500px] items-center">
                     <div className="relative bg-[#105372] flex items-center md:items-start flex-col overflow-hidden w-11/12 grow md:w-5/6 rounded-[40px] h-[550px]">
                         <div className="absolute md:right-[30px] top-[30px] z-50">
-                            <CollectionsButton title={"اکشن و ماجراجویی"} />
+                            <CollectionsButton path={'/Collection/Action'} title={"اکشن و ماجراجویی"} />
                         </div>
                         <img src="Images/book2.png" className="absolute bottom-0 z-0 w-full h-full object-cover" alt="action" />
                     </div>
@@ -21,13 +21,13 @@ export default function Collection() {
                     <div className="flex items-center flex-col md:w-full md:items-start md:flex-row gap-7">
                         <div className="relative bg-[#26292A] flex items-center md:items-start flex-col w-11/12 classic md:w-1/2 md:h-[260px] rounded-[40px] overflow-hidden">
                             <div className="absolute md:right-[30px] top-[30px]">
-                                <CollectionsButton title={"کلاسیک"} />
+                                <CollectionsButton path={'/Collection/Classical'} title={"کلاسیک"} />
                             </div>
                             <img className="w-full h-full object-cover" src="Images/book3.png" alt="classic" />
                         </div>
                         <div className="relative bg-[#FF6600] flex items-center flex-col w-11/12 md:w-1/2 md:h-[230px] overflow-hidden rounded-[40px]">
                             <div className="absolute md:right-[30px] top-[30px]">
-                                <CollectionsButton title={"کارگاه رمز و راز"} />
+                                <CollectionsButton path={'/Collection/Secret'} title={"کارگاه رمز و راز"} />
                             </div>
                             <img className="w-full h-full object-cover md:pr-10" src="Images/book1.png" alt="secret" />
                         </div>
@@ -35,14 +35,14 @@ export default function Collection() {
                     <div className="flex w-full flex-col items-center md:flex-row gap-7 mt-7 md:mt-0 ">
                         <div className="relative bg-[#758BFD] flex items-center md:items-start flex-col w-11/12 md:w-4/6 md:h-[260px] rounded-[40px] h-[250px] overflow-hidden">
                             <div className="absolute md:right-[30px] top-[30px] z-50">
-                                <CollectionsButton ny={true} title={"زمان هایی در نیویورک پر فروش ترین ها"} />
+                                <CollectionsButton path={'/Collection/Experience'} ny={true} title={"زمان هایی در نیویورک پر فروش ترین ها"} />
                             </div>
                             <img className="w-[500px] h-[250px] z-0 absolute top-[20px] lg:right-[50px] object-cover" src="Images/books2.png" alt="newYork" />
                         </div>
                         {/* bio */}
                         <div className="relative bg-[#F1F2F6] flex items-center md:items-start flex-col md:w-2/6 md:h-[260px] overflow-hidden w-11/12 rounded-[40px] h-[250px]">
                             <div className="absolute md:right-[30px] top-[30px] z-50">
-                                <CollectionsButton title={"بیوگرافی"} bio={true} />
+                                <CollectionsButton path={'/Collection/Biography'} title={"بیوگرافی"} bio={true} />
                             </div>
                             <img className="absolute bottom-0 z-0 left-0 w-[400px] md:w-full md:h-full object-cover" src="Images/biography.png" alt="biography" />
                         </div>
--- src/Components/CollectionsButton/CollectionsButton.jsx
+++ src/Components/CollectionsButton/CollectionsButton.jsx
@@ -1,15 +1,16 @@
 import { FaChevronLeft } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
-export default function CollectionsButton({ title, bio,ny }) {
+export default function CollectionsButton({ path,title, bio,ny }) {
     return (
         <div className="flex flex-col items-center z-50 gap-5">
             <div>
                 <h4 className={`${!bio ? "word text-white text-xl" : "word text-black text-xl"} ${ny && "word w-[200px] text-center"}`}>{title}</h4>
             </div>
-            <div className="border border-white cursor-pointer rounded-full py-1 px-1 text-white flex flex-row-reverse items-center gap-2 group hover:bg-white transition-all">
+            <Link to={path} className="border border-white cursor-pointer rounded-full py-1 px-1 text-white flex flex-row-reverse items-center gap-2 group hover:bg-white transition-all">
                 <span className="text-[16px] ml-2 group-hover:text-current regular">مشاهده مجموعه</span>
                 <span className="border rounded-full group-hover:bg-current text-[8px] p-2"><FaChevronLeft /></span>
-            </div>
+            </Link>
         </div>
     )
 }
